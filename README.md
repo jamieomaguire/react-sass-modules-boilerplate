8c5d3ff24b90a9583fe682736c15986968c61236
jamieomaguire <jamieolivermaguire@googlemail.com>
2017-04-22 20:38:55 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Boilerplate for React, Webpack2 and CSS-Modules
+A quick and easy React template for personal projects
---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import styles from './App.scss'
+
+export default class App extends Component {
+  render () {
+    return (
+      <div className={styles.container}>
+        <h1>Woot!</h1>
+      </div>
+    )
+  }
+}
